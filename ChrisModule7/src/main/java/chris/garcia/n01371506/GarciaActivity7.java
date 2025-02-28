2f05769f9fef199abe0bbeb8c0241bd5de6518ff
Chris Garcia <chrisgarcia911@outlook.com>
2025-02-28 11:56:43 -0500

    5) added tab navigation functionality

--- ChrisModule7/src/main/java/chris/garcia/n01371506/GarciaActivity7.java
+++ ChrisModule7/src/main/java/chris/garcia/n01371506/GarciaActivity7.java
@@ -6,12 +6,15 @@
 package chris.garcia.n01371506;
 
 import android.os.Bundle;
+import android.provider.Settings;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class GarciaActivity7 extends AppCompatActivity {
 
@@ -20,6 +23,33 @@ public class GarciaActivity7 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.BottomNav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new HomeFragment()).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedfragment = null;
+
+            if(item.getItemId() == R.id.home){
+                selectedfragment = new HomeFragment();
+            } else if (item.getItemId() == R.id.chris) {
+                selectedfragment = new ChrisFragment();
+            }else if (item.getItemId() == R.id.person) {
+                selectedfragment = new PersonFragment();
+            }else if (item.getItemId() == R.id.settings) {
+                selectedfragment = new SettingsFragment();
+            }
+
+            if(selectedfragment != null){
+               getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,selectedfragment).commit();
+
+            }
+
+
+            return true;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
